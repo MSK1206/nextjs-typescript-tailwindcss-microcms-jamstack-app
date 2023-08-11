3c088a1eb34e280a29e8f52b3d45e442c7011366
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-12 00:59:44 +0900

    Add: 記事の詳細ページを追加

---
+++ src/app/dynamic/[postId]/page.tsx
@@ -0,0 +1,41 @@
+import { notFound } from 'next/navigation'
+import parse from 'html-react-parser'
+import { getBlogDetail, getBlogList } from '@/app/_libs/microcms'
+
+// キャッシュを利用しない
+export const revalidate = 0
+
+export async function generateStaticParams() {
+  const { contents } = await getBlogList()
+
+  const paths = contents.map((blogs) => {
+    return {
+      blogsId: blogs.id
+    }
+  })
+
+  return [...paths]
+}
+
+export default async function StaticDetailPage({
+  params: { blogsId }
+}: {
+  params: { blogsId: string }
+}) {
+  const blogs = await getBlogDetail(blogsId)
+
+  // ページの生成された時間を取得
+  const time = new Date().toLocaleString()
+
+  if (!blogs) {
+    notFound()
+  }
+
+  return (
+    <div>
+      <h1>{blogs.title}</h1>
+      <h2>{time}</h2>
+      <div>{parse(blogs.content)}</div>
+    </div>
+  )
+}
