c0ccc6a8763f212df9efa9e7b884aefca4a9fcf2
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-13 00:04:53 +0900

    Add: BlogList import

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { getBlogList } from '@/app/_libs/microcms'
 import PublishedDate from '../_components/Date'
-import Image from 'next/image'
+import InfoTag from '@/app/_components/InfoTag'
+import BlogList from '../_components/BlogList'
 
 // キャッシュを利用しない => SSRと同等
 // キャッシュを〇秒間利用する => ISR同等
@@ -9,54 +11,11 @@ import Image from 'next/image'
 export const revalidate = 180
 
 export default async function Page() {
-  const { contents } = await getBlogList()
-
-  // ページの生成された時間を取得
-  const utcDate = new Date().toUTCString()
-
-  if (!contents || contents.length === 0) {
-    return <h1>No contents</h1>
-  }
+  const data = await getBlogList()
 
   return (
     <div className="flex items-center justify-center p-4">
-      <ul>
-        {contents.map((blogs) => {
-          return (
-            <li key={blogs.id}>
-              <Link href={`/blog/${blogs.id}`}>
-                {blogs.thumbnail ? (
-                  <Image
-                    src={blogs.thumbnail?.url}
-                    alt={blogs.title}
-                    className="w-80 h-auto"
-                    width={blogs.thumbnail?.width}
-                    height={blogs.thumbnail?.height}
-                  />
-                ) : (
-                  <Image
-                    src={'/no-img.jpg'}
-                    alt={'No-Image'}
-                    className=""
-                    width={1200}
-                    height={630}
-                  />
-                )}
-                <h1>{blogs.title}</h1>
-                {blogs.category.map((category) => (
-                  <li
-                    className="inline-flex items-center justify-between"
-                    key={category.id}
-                  >
-                    <span>{category.name}</span>
-                  </li>
-                ))}
-                <PublishedDate date={utcDate} />
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
+      <BlogList articles={data.contents} />
     </div>
   )
 }
