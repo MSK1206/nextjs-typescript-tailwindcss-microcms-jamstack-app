1ccc4725b5bead782e7aec37d7df25d1311a7e0a
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-12 17:24:03 +0900

    Fix: 記事取得リストページへPublishedDateコンポーネントを追加

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,42 +1,30 @@
 import Link from 'next/link'
 import { getBlogList } from '@/app/_libs/microcms'
-import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
-dayjs.extend(utc)
-dayjs.extend(timezone)
+import PublishedDate from '../_components/Date'
 
 // キャッシュを利用しない
 export const revalidate = 0
-
-export async function generateStaticParams() {
-  const { contents } = await getBlogList()
-
-  const paths = contents.map((blogs) => {
-    return {
-      blogsId: blogs.id
-    }
-  })
-
-  return [...paths]
-}
 
 export default async function Page() {
   const { contents } = await getBlogList()
 
   // ページの生成された時間を取得
-  const time = `${dayjs
-    .utc(new Date().toUTCString())
+  const utcDate = new Date().toUTCString()
+  {
+    /*const publishedDate = `${dayjs
+    .utc(utcDate)
     .tz('Asia/Tokyo')
-    .format('YYYY年MM月DD日')}`
+  .format('YYYY年MM月DD日')}`*/
+  }
 
   if (!contents || contents.length === 0) {
     return <h1>No contents</h1>
   }
 
   return (
-    <div className="">
-      <h1>{time}</h1>
+    <div className="flex items-center justify-center p-4">
+      {/* <h2>{publishedDate}</h2> */}
+      <PublishedDate date={utcDate} />
       <ul>
         {contents.map((blogs) => {
           return (
