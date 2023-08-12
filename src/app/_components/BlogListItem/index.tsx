a276e2a3165d85479007eb1b1c9769bcb42fe33a
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-12 23:57:34 +0900

    Add: 記事のリストを取得処理するコンポーネントを追加

---
+++ src/app/_components/BlogListItem/index.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { Blogs } from '@/app/_libs/microcms'
+import PublishedDate from '@/app/_components/Date'
+import InfoTag from '@/app/_components/InfoTag'
+
+type Props = {
+  blogs: Blogs
+}
+
+export default function BlogListItem({ blogs }: Props) {
+  // ページの生成された時間を取得
+  const utcDate = new Date().toUTCString()
+  return (
+    <li className="border-solid border-b border-b-gray-300 last:border-b-[0]">
+      <Link href={`/blog/${blogs.id}`} className="">
+        {blogs?.thumbnail ? (
+          <Image
+            src={blogs.thumbnail?.url}
+            alt={blogs.title}
+            className="w-[200px] h-auto rounded max-sm:hidden"
+            width={blogs.thumbnail?.width}
+            height={blogs.thumbnail?.height}
+          />
+        ) : (
+          <Image
+            src={'/no-img.jpg'}
+            alt={'No-Image'}
+            className="w-[200px] h-auto rounded max-sm:hidden"
+            width={1200}
+            height={630}
+          />
+        )}
+        <dl className="">
+          <dt className="text-[1rem] font-bold">{blogs.title}</dt>
+          <dd className="flex items-center gap-4">
+            <InfoTag infotag={blogs.infotag} />
+            <PublishedDate date={utcDate} />
+          </dd>
+        </dl>
+      </Link>
+    </li>
+  )
+}
