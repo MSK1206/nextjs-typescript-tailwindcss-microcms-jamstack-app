9e8ac992e9d4bf844389b2cbbca4f3c8e4b4e28c
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-12 13:37:35 +0900

    Fix: position: absolute => stickyに変更

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -4,7 +4,7 @@ import Menu from '@/app/_components/Menu'
 
 export default function Header() {
   return (
-    <header className="flex absolute items-center justify-between w-full bg-slate-500">
+    <header className="flex sticky items-center justify-between w-full bg-slate-500">
       <Link href={'/'}>
         <Brand />
       </Link>
