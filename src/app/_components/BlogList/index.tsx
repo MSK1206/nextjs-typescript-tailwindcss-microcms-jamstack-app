743da76e05cb4f9ff7a3d815609fc11dfb3e509c
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-12 23:59:05 +0900

    Add: 取得した記事のリストの表示するコンポーネントを追加

---
+++ src/app/_components/BlogList/index.tsx
@@ -0,0 +1,19 @@
+import { Blogs } from '@/app/_libs/microcms'
+import BlogListItem from '@/app/_components/BlogListItem'
+
+type Props = {
+  articles?: Blogs[]
+}
+
+export default function BlogList({ articles }: Props) {
+  if (!articles || articles.length === 0) {
+    return <p>記事がありません。</p>
+  }
+  return (
+    <ul>
+      {articles.map((article) => (
+        <BlogListItem key={article.id} blogs={article} />
+      ))}
+    </ul>
+  )
+}
