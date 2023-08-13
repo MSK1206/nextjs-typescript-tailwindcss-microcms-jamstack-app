fdf64df7bed3d3f8cd95eb6e7d4995ac5ad47161
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-13 17:58:03 +0900

    Add: 記事リストページにPaginationを追加

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { getBlogList } from '@/app/_libs/microcms'
 import { BLOG_LIST_LIMIT } from '../_constants'
-import BlogList from '../_components/BlogList'
+import BlogList from '@/app/_components/BlogList'
+import Pagination from '@/app/_components/Pagination'
 
 // キャッシュを利用しない => SSRと同等
 // キャッシュを〇秒間利用する => ISR同等
@@ -17,6 +18,7 @@ export default async function Page() {
   return (
     <div className="flex items-center justify-center p-4">
       <BlogList articles={data.contents} />
+      <Pagination totalCount={data.totalCount} />
     </div>
   )
 }
