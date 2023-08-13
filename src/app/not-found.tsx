e2ded0c27dc5f31b6b713836ea135c7fd363ff3a
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-13 16:23:03 +0900

    Fix: 画面中央に表示させる

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <>
-      <div>
-        <h1>ページが見つかりませんでした。</h1>
+      <div className="flex items-center justify-center">
+        <h1 className="font-bold">ページが見つかりませんでした。</h1>
       </div>
     </>
   )
