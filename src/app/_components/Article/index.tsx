cc25895b6f461b75821d2747b80ac9ff9045607b
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-13 21:04:03 +0900

    Add: Article

---
+++ src/app/_components/Article/index.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import { type Blogs } from '@/app/_libs/microcms'
+import PublishedDate from '@/app/_components/Date'
+import parse from 'html-react-parser'
+import Category from '@/app/_components/Categories'
+
+type Props = {
+  data: Blogs
+}
+
+export default function Article({ data }: Props) {
+  return (
+    <main>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+      <div>
+        <Category category={data.category} />
+        <PublishedDate date={data.publishedAt || data.createdAt} />
+      </div>
+      {data.thumbnail && (
+        <Image
+          src={data.thumbnail?.url}
+          alt=""
+          width={data.thumbnail?.width}
+          height={data.thumbnail?.height}
+        />
+      )}
+      <div>{parse(data.content)}</div>
+    </main>
+  )
+}
