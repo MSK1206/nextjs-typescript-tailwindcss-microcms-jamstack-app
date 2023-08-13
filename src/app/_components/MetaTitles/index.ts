d2247dcbc2a7ea7c3d967d8cd2788ad2118680f8
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-13 16:14:58 +0900

    Add: MetaTitles.ts タイトルやページ名の定義ファイル追加

---
+++ src/app/_components/MetaTitles/index.ts
@@ -0,0 +1,13 @@
+// title | titleTemplate
+
+// titleTemplate
+export const MetaTitleTemplate = 'Re:birth'
+
+// Root Page title
+export const MetaRootPageTitle = 'Home'
+
+// Dynamic Page (blog/page.tsx and blog/[blogsId]/page.tsx) title
+export const MetaDynamicTitle = 'Blog'
+
+// Contact Page title
+export const MetaContactTitle = 'Contact'
