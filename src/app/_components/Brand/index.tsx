88b23501c22b8a14ada24754fd6806ddd88cc098
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-13 16:13:24 +0900

    Fix: MetaTitlesからtitle,titleTemplateをImportして取得に変更

--- src/app/_components/Brand/index.tsx
+++ src/app/_components/Brand/index.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image'
+import { MetaTitleTemplate as BlandName } from '@/app/_components/MetaTitles'
 
 export default function Brand() {
   return (
     <div className="inline-flex items-center justify-between p-1">
       <Image
         src={'/mylogo.svg'}
-        alt="BrandLogo"
+        alt={`${BlandName} Logo`}
         className="h-10 w-auto"
         width={40}
         height={40}
         priority
       />
-      <span className="">Matsushita Web Design</span>
+      <span className="">{BlandName}</span>
     </div>
   )
 }
