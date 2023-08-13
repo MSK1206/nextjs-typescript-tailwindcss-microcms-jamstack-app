cb8de96705e358d3fa7d98adea65c5fadc60f6f8
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-14 02:06:05 +0900

    Fix: Sheet style変更

--- src/app/_components/Sheet/index.tsx
+++ src/app/_components/Sheet/index.tsx
@@ -1,11 +1,9 @@
+import styles from './index.module.css'
+
 type Props = {
   children: React.ReactNode
 }
 
 export default function Sheet({ children }: Props) {
-  return (
-    <div className="flex items-center justify-center bg-white w-[840px] m-[-40px,auto,0] p-20 rounded-[8px] max-sm:w-10/12 max-sm:p-[40px,auto] max-md:w-9/12">
-      {children}
-    </div>
-  )
+  return <div className={styles.container}>{children}</div>
 }
