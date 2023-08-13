8b087fd35cd13b6f0211f263e4729d75e453d61f
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-13 17:29:58 +0900

    Add: Pagination

---
+++ src/app/_components/Pagination/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+type Props = {
+  totalCount: number
+}
+
+export default function Pagination({ totalCount }: Props) {
+  return (
+    <>
+      <ul>
+        <li></li>
+      </ul>
+    </>
+  )
+}
