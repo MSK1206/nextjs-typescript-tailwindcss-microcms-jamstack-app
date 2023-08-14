58959144311429fdd5c266b7e3ebe1b98cb26c68
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-14 12:48:16 +0900

    Fix: swap icon

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -4,13 +4,12 @@ import Menu from '@/app/_components/Menu'
 
 export default function Header() {
   return (
-    <header className="navbar">
+    <header className="navbar sticky">
       <div className="navbar-start">
         <Link href={'/'}>
           <Brand />
         </Link>
       </div>
-
       <div className="navbar-end">
         <Menu />
       </div>
--- src/app/_components/Menu/index.tsx
+++ src/app/_components/Menu/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Bars3BottomRightIcon } from '@heroicons/react/24/outline'
+import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 export default function Menu() {
   return (
@@ -14,15 +14,14 @@ export default function Menu() {
           >
             {/* this hidden checkbox controls the state */}
             <input type="checkbox" />
-            <Bars3BottomRightIcon className="swap-off fill-current w-8 h-8 text-black" />
+            <Bars3BottomRightIcon className="swap-off w-8 h-8 text-black" />
           </label>
         </div>
         <div className="drawer-side">
-          <label htmlFor="my-drawer-4" className="drawer-overlay">
-            <Bars3BottomRightIcon className="swap-on fill-current w-8 h-8 text-black" />
-          </label>
+          <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
             {/* Sidebar content here */}
+            <XMarkIcon className="swap-on w-8 h-8 text-black absolute top-1 right-1" />
             <li>
               <Link href={'/'}>Home</Link>
             </li>
