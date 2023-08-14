222ebd6ed23cc1dcd63be0d27031830cae5851d3
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-14 12:26:24 +0900

    Add: dropdownからdrawerメニューへ変更

--- src/app/_components/Menu/index.tsx
+++ src/app/_components/Menu/index.tsx
@@ -1,26 +1,39 @@
 import Link from 'next/link'
-
-const MenuItems = ['Home', 'Blog', 'Contact']
+import { Bars3BottomRightIcon } from '@heroicons/react/24/outline'
 
 export default function Menu() {
   return (
     <>
-      <div className="dropdown">
-        <label tabIndex={0} className="btn btn-ghost btn-circle"></label>
-        <ul
-          tabIndex={0}
-          className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <Link href={'/'}>Home</Link>
-          </li>
-          <li>
-            <Link href={'/blog'}>Blog</Link>
-          </li>
-          <li>
-            <Link href={'/contact'}>Contact</Link>
-          </li>
-        </ul>
+      <div className="drawer drawer-end">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* Page content here */}
+          <label
+            htmlFor="my-drawer-4"
+            className="drawer-button btn btn-ghost btn-circle swap swap-rotate"
+          >
+            {/* this hidden checkbox controls the state */}
+            <input type="checkbox" />
+            <Bars3BottomRightIcon className="swap-off fill-current w-8 h-8 text-black" />
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-4" className="drawer-overlay">
+            <Bars3BottomRightIcon className="swap-on fill-current w-8 h-8 text-black" />
+          </label>
+          <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            <li>
+              <Link href={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link href={'/blog'}>Blog</Link>
+            </li>
+            <li>
+              <Link href={'/contact'}>Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   )
