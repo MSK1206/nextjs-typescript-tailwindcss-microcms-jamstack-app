190d39cb54fd86c2ef9b7e797f8d4c783e5d4b75
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-14 17:41:13 +0900

    Fix: Menu

--- src/app/_components/Menu/index.tsx
+++ src/app/_components/Menu/index.tsx
@@ -1,40 +1,47 @@
+'use client'
 import Link from 'next/link'
+import { useState } from 'react'
 import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import cx from 'classnames'
+import styles from './index.module.css'
+
+interface MenuItem {
+  label: string
+  link: string
+}
+
+const menuItems: MenuItem[] = [
+  { label: 'Home', link: '/' },
+  { label: 'About', link: '/about' },
+  { label: 'Blogs', link: '/blog' },
+  { label: 'Contact', link: '/contact' }
+]
 
 export default function Menu() {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const toggleMenu = () => {
+    setIsOpen((prevOpen) => !prevOpen)
+  }
   return (
-    <>
-      <div className="dropdown-right">
-        <label
-          tabIndex={0}
-          className="btn btn-ghost btn-circle swap swap-rotate"
-        >
-          {/* this hidden checkbox controls the state */}
-          <input type="checkbox" />
-
-          {/* hamburger icon */}
-          <Bars3BottomRightIcon className="swap-off fill-current w-8 h-8 text-black" />
-          {/* close icon */}
-          <XMarkIcon className="swap-on fill-current w-8 h-8 text-black" />
-        </label>
-        <ul
-          tabIndex={0}
-          className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <Link href={'/'}>Home</Link>
-          </li>
-          <li>
-            <Link href={'/about'}>About</Link>
-          </li>
-          <li>
-            <Link href={'/blog'}>Blogs</Link>
-          </li>
-          <li>
-            <Link href={'/Contact'}>Contact</Link>
-          </li>
+    <div>
+      <nav className={cx(styles.nav, isOpen && styles.open)}>
+        <ul className={styles.items}>
+          {menuItems.map((item) => (
+            <li className="menu-item" key={item.label}>
+              <Link href={item.link}>{item.label}</Link>
+            </li>
+          ))}
         </ul>
-      </div>
-    </>
+        <button
+          className={cx(styles.button, styles.close)}
+          onClick={toggleMenu}
+        >
+          <XMarkIcon className="w-8 h-8 text-white" />
+        </button>
+      </nav>
+      <button className={styles.button} onClick={toggleMenu}>
+        <Bars3BottomRightIcon className="w-8 h-8 text-black" />
+      </button>
+    </div>
   )
 }
