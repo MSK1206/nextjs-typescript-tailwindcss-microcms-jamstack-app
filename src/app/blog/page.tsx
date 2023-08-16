baf986ae574ee84eb19b88ce6456298fe42326d1
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-17 02:30:33 +0900

    Del: Props削除

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,18 +3,12 @@ import { BLOG_LIST_LIMIT } from '../_constants'
 import BlogList from '@/app/_components/BlogList'
 import Pagination from '@/app/_components/Pagination'
 
-type Props = {
-  params: {
-    current: string
-  }
-}
-
 // キャッシュを利用しない => SSRと同等
 // キャッシュを〇秒間利用する => ISR同等
 
 export const revalidate = 60
 
-export default async function Page({ params }: Props) {
+export default async function Page() {
   const data = await getBlogList({
     limit: BLOG_LIST_LIMIT
   })
