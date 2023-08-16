11de6ae5d8cb69a9f904409b64cf068c001a81a9
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-16 22:19:46 +0900

    Fix:

--- src/app/blog/p/[current]/page.tsx
+++ src/app/blog/p/[current]/page.tsx
@@ -23,7 +23,7 @@ export default async function Page({ params }: Props) {
   })
 
   return (
-    <div className="flex items-center justify-center p-4">
+    <div className="p-4">
       <BlogList articles={data.contents} />
       <Pagination
         totalCount={data.totalCount}
