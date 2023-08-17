d45a872b378a297a85dbfd79dec2509effa68b3d
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-18 01:45:54 +0900

    Fix: padding class 修正

--- src/app/_components/InfoTag/index.tsx
+++ src/app/_components/InfoTag/index.tsx
@@ -5,5 +5,5 @@ type Props = {
 }
 
 export default function InfoTag({ infotag }: Props) {
-  return <span className="pt-1 pb-1 pr-3 pl-3">{infotag.name}</span>
+  return <span className="pt-1 pb-1 pr-2">{infotag.name}</span>
 }
