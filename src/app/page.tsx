f9b21ffb5d6dc3c25abf38df417fdb43a42e5f2b
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-18 01:42:15 +0900

    Add: hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Developments</h1>
-    </main>
+    <>
+      <section className="text-white flex items-center justify-center overflow-hidden">
+        <div className="mt-[40px]">
+          <h1 className="text-3xl font-bold"></h1>
+          <p className=""></p>
+        </div>
+        <Image
+          className="absolute top-0 right-0 h-[300px] bg-cover flex items-center justify-center z-[-3] max-sm:h-[150px]"
+          src="/hero.jpg"
+          alt=""
+          width={3600}
+          height={1200}
+          priority
+        />
+      </section>
+      <section className="mt-[170px] p-[200px] max-sm:p-[40px] max-sm:mt-[50px]">
+        <div className="">
+          <h2></h2>
+          <p></p>
+        </div>
+      </section>
+    </>
   )
 }
