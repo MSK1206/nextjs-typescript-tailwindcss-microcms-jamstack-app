f6ce84b703a0bc5a830fe3c31847585dfa939570
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-18 01:43:41 +0900

    Fix: image コメントアウト

--- src/app/_components/Brand/index.tsx
+++ src/app/_components/Brand/index.tsx
@@ -4,14 +4,14 @@ import { MetaTitleTemplate as BlandName } from '@/app/_components/MetaTitles'
 export default function Brand() {
   return (
     <div className="inline-flex items-center justify-between p-1">
-      <Image
+      {/*<Image
         src={'/mylogo.svg'}
         alt={`${BlandName} Logo`}
         className="h-10 w-auto mr-2"
         width={40}
         height={40}
         priority
-      />
+      />*/}
       <span className="text-lg">{BlandName}</span>
     </div>
   )
