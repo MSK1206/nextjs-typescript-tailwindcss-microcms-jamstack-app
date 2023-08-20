2a8e53b0b1620962eec75641c406573ed16c8981
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-21 01:19:40 +0900

    Add: X-Frame-Options & X-Content-Type-Options

--- next.config.js
+++ next.config.js
@@ -7,6 +7,46 @@ const nextConfig = {
         hostname: 'images.microcms-assets.io'
       }
     ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/contact',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          }
+        ]
+      },
+      {
+        source: '/blog',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      },
+      {
+        source: '/blog/p/:current*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      },
+      {
+        source: '/blog/:slug*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
+        ]
+      }
+    ]
   }
 }
 
