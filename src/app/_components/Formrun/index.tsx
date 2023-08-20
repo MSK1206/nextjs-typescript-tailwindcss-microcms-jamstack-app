07c12408a86efacdf52a283e0415b963e9d5f228
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-20 15:54:06 +0900

    Fix: script.url

--- src/app/_components/Formrun/index.tsx
+++ src/app/_components/Formrun/index.tsx
@@ -4,17 +4,16 @@ import styles from './index.module.css'
 
 export default function Formrun() {
   const FormrunID: string = `${process.env.NEXT_PUBLIC_FORMRUN_FORM_ID}`
-  const FormrunURL: string = `${process.env.NEXT_PUBLIC_FORMRUN_FORM_URL}`
   useEffect(() => {
     const head = document.getElementsByTagName('head')[0] as HTMLElement
     const scriptUrl = document.createElement('script')
-    scriptUrl.src = FormrunURL
+    scriptUrl.src = 'https://sdk.form.run/js/v2/embed.js'
     head.appendChild(scriptUrl)
     return () => {
       // コンポーネントがアンマウントされる際にHeadからスクリプトのクリーンアップを行う
       head.removeChild(scriptUrl)
     }
-  }, [FormrunURL])
+  }, [])
   return (
     <>
       <div className={styles.formrun_screen}>
