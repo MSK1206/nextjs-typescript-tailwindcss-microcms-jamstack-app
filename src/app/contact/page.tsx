87febf88c5111e68eefba2ba27c2901a7f314d17
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-20 16:22:43 +0900

    Fix: Some fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,18 +1,6 @@
-'use client'
-import { useEffect } from 'react'
 import Formrun from '@/app/_components/Formrun'
 
 export default function Page() {
-  useEffect(() => {
-    const head = document.getElementsByTagName('head')[0] as HTMLElement
-    const scriptUrl = document.createElement('script')
-    scriptUrl.src = 'https://sdk.form.run/js/v2/embed.js'
-    head.appendChild(scriptUrl)
-    return () => {
-      // コンポーネントがアンマウントされる際にHeadからスクリプトのクリーンアップを行う
-      head.removeChild(scriptUrl)
-    }
-  }, [])
   return (
     <div className="min-h-screen z-[10]">
       <Formrun />
