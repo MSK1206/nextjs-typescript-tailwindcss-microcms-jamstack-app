3f00b8c65958c221d2ba0ae966449e15746001f0
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-20 15:57:23 +0900

    Fix: use client

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,9 @@
+'use client'
 import Formrun from '../_components/Formrun'
 
 export default function Page() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen z-[10]">
       <Formrun />
     </div>
   )
