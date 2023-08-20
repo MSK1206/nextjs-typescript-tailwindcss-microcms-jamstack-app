08fcf0b0ca7f6099baeb026b9b59bd9b4b48c47e
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-20 16:15:19 +0900

    Add: useEffect

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,18 @@
 'use client'
-import Formrun from '../_components/Formrun'
+import { useEffect } from 'react'
+import Formrun from '@/app/_components/Formrun'
 
 export default function Page() {
+  useEffect(() => {
+    const head = document.getElementsByTagName('head')[0] as HTMLElement
+    const scriptUrl = document.createElement('script')
+    scriptUrl.src = 'https://sdk.form.run/js/v2/embed.js'
+    head.appendChild(scriptUrl)
+    return () => {
+      // コンポーネントがアンマウントされる際にHeadからスクリプトのクリーンアップを行う
+      head.removeChild(scriptUrl)
+    }
+  }, [])
   return (
     <div className="min-h-screen z-[10]">
       <Formrun />
