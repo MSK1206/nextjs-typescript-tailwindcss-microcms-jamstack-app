84ae599757ae887f017b150543524f5ae650d19e
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-20 16:16:04 +0900

    Fix: useEffect処理を分離

--- src/app/_components/Formrun/index.tsx
+++ src/app/_components/Formrun/index.tsx
@@ -4,16 +4,6 @@ import styles from './index.module.css'
 
 export default function Formrun() {
   const FormrunID: string = `${process.env.NEXT_PUBLIC_FORMRUN_FORM_ID}`
-  useEffect(() => {
-    const head = document.getElementsByTagName('head')[0] as HTMLElement
-    const scriptUrl = document.createElement('script')
-    scriptUrl.src = 'https://sdk.form.run/js/v2/embed.js'
-    head.appendChild(scriptUrl)
-    return () => {
-      // コンポーネントがアンマウントされる際にHeadからスクリプトのクリーンアップを行う
-      head.removeChild(scriptUrl)
-    }
-  }, [])
   return (
     <>
       <div className={styles.formrun_screen}>
