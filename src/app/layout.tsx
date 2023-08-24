408576ac415035391873481cf805b43378abfbc8
MSK <86665622+MSK1206@users.noreply.github.com>
2023-08-25 05:43:20 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,6 @@
 import './globals.css'
 import Header from '@/app/_components/Header'
 import Footer from '@/app/_components/Footer'
-import { Metadata } from 'next';
-import { getMeta } from '@/app/_libs/microcms'
-import { MetaTitleTemplate as TitleTemplate } from '@/app/_components/MetaTitles'
-
-export const revalidate = 60;
-
-export async function generateMetadata(): Promise<Metadata> {
-  const data = await getMeta()
-  if (!data) {
-    return {}
-  }
-
-  return {
-    metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
-    title: data.titleTemplate,
-    description: data.description,
-    openGraph: {
-      type: 'website',
-      title: data.ogTitle,
-      description: data.ogDescription,
-      siteName: `${TitleTemplate}`,
-      images: [`${data?.ogImage?.url}?txt=${data.ogDescription}&txt-size=40&txt-align=middle,center` || '']
-    },
-    alternates: {
-      canonical: data.canonical
-    },
-    twitter: {
-    card: 'summary_large_image'
-    }  
-  }
-}
 
 export default function RootLayout({
   children
