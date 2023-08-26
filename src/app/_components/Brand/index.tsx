c8fbfd384c8464ebaf530e43aa7073a325b488c8
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-26 19:48:32 +0900

    Add: imge style responsive

--- src/app/_components/Brand/index.tsx
+++ src/app/_components/Brand/index.tsx
@@ -4,15 +4,15 @@ import { MetaTitleTemplate as BlandName } from '@/app/_components/MetaTitles'
 export default function Brand() {
   return (
     <div className="inline-flex items-center justify-between p-1">
-      {/*<Image
+      <Image
         src={'/mylogo.svg'}
         alt={`${BlandName} Logo`}
-        className="h-10 w-auto mr-2"
+        className="h-10 w-auto mr-2 sm:hidden"
         width={40}
         height={40}
         priority
-      />*/}
-      <span className="text-lg">{BlandName}</span>
+      />
+      <span className="text-lg max-[768px]:hidden">{BlandName}</span>
     </div>
   )
 }
