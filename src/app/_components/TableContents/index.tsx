20213e7cf107cf7bea23957f2f0fefcff09f38f4
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-02 03:01:05 +0900

    Add: 目次

---
+++ src/app/_components/TableContents/index.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  tablContentsTtitle: string
+}
+
+export default function TableContents({ tablContentsTtitle }: Props) {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl pt-2 pb-2">{tablContentsTtitle}</h1>
+        <span className="pt-2 pb-2 w-[100%]">
+          <hr />
+        </span>
+      </div>
+    </>
+  )
+}
