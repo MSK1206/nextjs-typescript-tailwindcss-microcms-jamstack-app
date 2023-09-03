23c9bb7e98f51d4e345d3a803c08523e151a1e20
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-03 16:05:17 +0900

    Add: aria-hidden

--- src/app/_components/TableContents/index.tsx
+++ src/app/_components/TableContents/index.tsx
@@ -7,7 +7,7 @@ export default function TableContents({ tablContentsTtitle }: Props) {
     <>
       <div>
         <h1 className="text-2xl pt-2 pb-2">{tablContentsTtitle}</h1>
-        <span className="pt-2 pb-2 w-[100%]">
+        <span className="pt-2 pb-2 w-[100%]" aria-hidden={true}>
           <hr />
         </span>
       </div>
