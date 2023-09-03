f09460ac6f799b6e8ccdbd6ef733b69f27095fb7
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-03 16:00:11 +0900

    Fix: 型定義

--- src/app/_components/Rating/index.tsx
+++ src/app/_components/Rating/index.tsx
@@ -1,4 +1,4 @@
-interface RatingProps {
+type RatingProps = {
   rating: number
 }
 
@@ -6,10 +6,10 @@ export default function Rating({ rating }: RatingProps) {
   const maxRating = 5 // 最大評価の数
   const filledStars = Math.min(Math.max(rating, 1), maxRating) // 最小1、最大5の評価を設定
 
-  // 評価を星で表示するための文字列を生成
+  // 評価を星で5段階で表示するための文字列を生成
   const stars = Array.from({ length: maxRating }, (_, index) => {
     if (index < filledStars) {
-      return '★' // フィルドされた星
+      return '★' // レーティングされた星
     } else {
       return '☆' // 空の星
     }
@@ -17,7 +17,7 @@ export default function Rating({ rating }: RatingProps) {
 
   return (
     <div>
-      <span>{stars}</span>
+      <span className="text-yellow-300">{stars}</span>
     </div>
   )
 }
