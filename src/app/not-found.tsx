b4b97bad266572272da456e3453f52dcef060b2a
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-03 15:31:28 +0900

    Add: hero

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,13 @@
+import Hero from '@/app/_components/Hero'
+
 export default function NotFound() {
   return (
     <>
+      <Hero title={'404'} sub={'Not Found'} />
       <div className="flex min-h-screen items-center justify-center">
-        <h1 className="font-bold text-2xl">ページが見つかりませんでした。</h1>
+        <h1 className="font-bold text-2xl" role="alert">
+          ページが見つかりませんでした。
+        </h1>
       </div>
     </>
   )
