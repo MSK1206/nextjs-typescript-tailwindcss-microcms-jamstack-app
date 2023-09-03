91e70801829f2ca2a41d949e3f926629fa3513e4
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-04 01:42:12 +0900

    Fix:

--- src/app/_components/Formrun/index.tsx
+++ src/app/_components/Formrun/index.tsx
@@ -4,12 +4,13 @@ import styles from './index.module.css'
 
 export default function Formrun() {
   const FormrunID: string = `${process.env.NEXT_PUBLIC_FORMRUN_FORM_ID}`
-  const preconnectLink = document.createElement('link')
-  preconnectLink.rel = 'preconnect'
-  preconnectLink.href = 'https://sdk.form.run/js/v2/embed.js'
-  // <Head>に<link>を追加してサーバーに事前に接続
-  document.head.appendChild(preconnectLink)
   useEffect(() => {
+    const preconnectLink = document.createElement('link')
+    preconnectLink.rel = 'preconnect'
+    preconnectLink.href = 'https://sdk.form.run/js/v2/embed.js'
+    // <Head>に<link>を追加してサーバーに事前に接続
+    document.head.appendChild(preconnectLink)
+
     const head = document.getElementsByTagName('head')[0] as HTMLElement
     const scriptUrl = document.createElement('script')
     scriptUrl.src = 'https://sdk.form.run/js/v2/embed.js'
