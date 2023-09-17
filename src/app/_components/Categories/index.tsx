bff0e43284b2f3a163c0238ce57fdc4e52ee8c66
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-17 23:55:42 +0900

    Fix:

--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -6,15 +6,15 @@ type Props = {
 
 export default function Category({ category }: Props) {
   return (
-    <>
+    <ul>
       {category.map((category) => (
         <li
           className="inline-flex items-center justify-between"
           key={category.id}
         >
-          <span className="pr-1 pl-1">{category.name}</span>
+          <span className="pr-1 pl-1 first:pl-0">{category.name}</span>
         </li>
       ))}
-    </>
+    </ul>
   )
 }
