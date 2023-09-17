4f95bdfecff4e7a4281d1590a68af8011a05d82f
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-17 23:52:29 +0900

    Add: TagIcon and responsive

--- src/app/_components/Article/index.tsx
+++ src/app/_components/Article/index.tsx
@@ -3,7 +3,7 @@ import { type Blogs } from '@/app/_libs/microcms'
 import PublishedDate from '@/app/_components/Date'
 import parse from 'html-react-parser'
 import Category from '@/app/_components/Categories'
-import cx from 'classnames'
+import { TagIcon } from '@heroicons/react/24/outline'
 
 type Props = {
   data: Blogs
@@ -12,11 +12,18 @@ type Props = {
 export default function Article({ data }: Props) {
   return (
     <main id="top" role="main">
-      <h1 className={'text-center font-bold text-[20px]'}>{data.title}</h1>
-      <p className={cx('text-center mt-1', {})}>{data.description}</p>
+      <h1
+        className={
+          'text-center font-bold text-[20px] max-[796px]:text-[calc(100%+5px)] max-[844px]:text-[calc(100%+10px)] max-[896px]:text-[calc(100%+15px)] max-[1366px]:text-[calc(100%+25px)]'
+        }
+      >
+        {data.title}
+      </h1>
+      <p className={'text-center mt-2'}>{data.description}</p>
       <br />
       <br />
-      <div className={cx('', {})}>
+      <div className={'flex items-center'}>
+        <TagIcon className="h-4 w-4 text-black" />
         <Category category={data.category} />
       </div>
       <PublishedDate date={data.publishedAt || data.createdAt} />
@@ -24,13 +31,13 @@ export default function Article({ data }: Props) {
         <Image
           src={data.thumbnail?.url}
           alt=""
-          className={cx('pt-2 pb-2', {})}
+          className={'pt-2 pb-2'}
           width={data.thumbnail?.width}
           height={data.thumbnail?.height}
           role="img"
         />
       )}
-      <div className={cx('', {})} role="article">
+      <div className={''} role="article">
         {parse(data.content)}
       </div>
     </main>
