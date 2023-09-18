8503353c6b2d35e0a21e5fd049af3b6bf827df69
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-18 23:24:37 +0900

    Fix: spanのテキストが改行され内容に修正

--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -1,4 +1,5 @@
 import { Categories } from '@/app/_libs/microcms'
+import { TagIcon } from '@heroicons/react/24/outline'
 
 type Props = {
   category: Categories[]
@@ -6,13 +7,13 @@ type Props = {
 
 export default function Category({ category }: Props) {
   return (
-    <ul>
+    <ul className="flex items-center">
+      <TagIcon className="h-4 w-4 text-black" />
       {category.map((category) => (
-        <li
-          className="inline-flex items-center justify-between"
-          key={category.id}
-        >
-          <span className="pr-1 pl-1 first:pl-0">{category.name}</span>
+        <li className="flex items-center" key={category.id}>
+          <span className="text-xs border rounded-[8px] pt-1 pr-2 pb-1 pl-2 ml-1 whitespace-nowrap max-[796px]:text-[8px]">
+            {category.name}
+          </span>
         </li>
       ))}
     </ul>
