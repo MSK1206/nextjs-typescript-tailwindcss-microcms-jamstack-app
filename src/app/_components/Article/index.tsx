73bd02dd3f3e5e533f9f590c12853afde870cb65
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-19 02:15:44 +0900

    Fix: カテゴリーと投稿日時を先頭に変更

--- src/app/_components/Article/index.tsx
+++ src/app/_components/Article/index.tsx
@@ -11,19 +11,20 @@ type Props = {
 export default function Article({ data }: Props) {
   return (
     <main id="top" role="main">
-      <h1 className="text-center font-bold text-[20px] max-[796px]:text-[calc(100%+5px)] max-[844px]:text-[calc(100%+10px)] max-[896px]:text-[calc(100%+15px)] max-[1366px]:text-[calc(100%+25px)]">
+      <div className="">
+        <Category category={data.category} />
+      </div>
+      <PublishedDate date={data.publishedAt || data.createdAt} />
+      <h1 className="mt-6 text-center font-bold text-[20px] max-[796px]:text-[calc(100%+5px)] max-[844px]:text-[calc(100%+10px)] max-[896px]:text-[calc(100%+15px)] max-[1366px]:text-[calc(100%+25px)]">
         {data.title}
       </h1>
       <p className="text-center mt-2">{data.description}</p>
       <br />
       <br />
-      <div className="">
-        <Category category={data.category} />
-      </div>
-      <PublishedDate date={data.publishedAt || data.createdAt} />
+
       {data.thumbnail && (
         <Image
-          src={data.thumbnail?.url}
+          src={`${data.thumbnail?.url}?txt=${data.title}&txt-size=45&txt-align=middle,center&txt-shad=5`}
           alt={data.title}
           className={'pt-2 pb-2'}
           width={data.thumbnail?.width}
