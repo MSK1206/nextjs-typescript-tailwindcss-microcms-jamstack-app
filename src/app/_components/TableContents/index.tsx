4e74d450de9eeb2862b02bab645f73dafd2e1f60
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-18 23:26:06 +0900

    Fix: h1のfont-weight修正

--- src/app/_components/TableContents/index.tsx
+++ src/app/_components/TableContents/index.tsx
@@ -6,7 +6,7 @@ export default function TableContents({ tablContentsTtitle }: Props) {
   return (
     <>
       <div>
-        <h1 className="text-2xl pt-2 pb-2">{tablContentsTtitle}</h1>
+        <h1 className="text-2xl font-bold pt-2 pb-2">{tablContentsTtitle}</h1>
         <span className="pt-2 pb-2 w-[100%]" aria-hidden={true}>
           <hr />
         </span>
