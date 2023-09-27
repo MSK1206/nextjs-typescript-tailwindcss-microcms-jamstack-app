77b96163d0af1847425ee96a83eb466a1b3e707a
MSK1206 <phantom7216@yahoo.co.jp>
2023-09-28 01:08:36 +0900

    Add: Category Title

--- src/app/_components/MetaTitles/index.ts
+++ src/app/_components/MetaTitles/index.ts
@@ -6,8 +6,11 @@ export const MetaTitleTemplate = 'MSK1206'
 // Root Page title
 export const MetaRootPageTitle = 'Home'
 
-// Dynamic Page (blog/page.tsx) title
+// Dynamic Page (/blog/page.tsx) title
 export const MetaDynamicTitle = 'Blog'
+
+// Dynamic Page (/category/page.tsx) title
+export const MetaDynamicCategoryTitle = 'Category'
 
 // Profile Page title
 export const MetaProfileTitle = 'Profile'
